f0348f4dd5e971dd8c9f73c3b363d3a34197c39f
CanDenizGokgedik <20050111057@ybu.edu.tr>
2023-01-21 12:05:02 +0300

    main 1

---
+++ src/main/Client.java
@@ -0,0 +1,16 @@
+package main;
+
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class Client {
+
+	public static void main(String[] args) {
+		
+		Socket socket = null;
+		InputStreamReader inputStreamReader = null;
+		OutputStreamWriter outputStreamWriter = null;
+		
+	}
+}
